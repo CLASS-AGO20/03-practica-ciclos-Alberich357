eeb299089f343cbbaa93a34fb47c62b410c4a1bc
Alberich LLerenas <allerenas4@ucol.mx>
2020-12-10 16:19:21 -0600

    implemented factorial

--- src/main.js
+++ src/main.js
@@ -1,3 +1,15 @@
-export default class App {}
+export default class App {
+    
+    factorial(numero) {
+        let i,total = 1; 
+        for (i=1; i<=numero; i++) {
+            total = total * i; 
+        }
+        return total; 
+    }
+}
 
 let app = new App();
+
+console.log("Funcion factorial de un numero:");
+console.log(app.factorial(5));
