c21faf078608610f98749144feab35fc9c04c324
Alberich LLerenas <allerenas4@ucol.mx>
2020-12-10 16:26:42 -0600

    implemented convertir a string

--- src/main.js
+++ src/main.js
@@ -7,9 +7,20 @@ export default class App {
         }
         return total; 
     }
+    convertirAString(numero){
+        let i=numero;
+        let string = "";
+        while(i > 0){
+            string += '*';
+            i--;
+       }
+       return string;
+    }
 }
 
 let app = new App();
 
 console.log("Funcion factorial de un numero:");
 console.log(app.factorial(5));
+console.log("Funcion convertir numero a string: ");
+console.log(app.convertirAString(5));
